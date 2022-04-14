1e1ff93b053a6a7c103369fca5e05ed69f27efeb
waterbeen0530 <hange253@gmail.com>
2022-04-14 20:35:25 +0900

    [UPDATE] Add css
    css 추가

--- src/components/Explanation.jsx
+++ src/components/Explanation.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import styles from "../styles/Explanation.module.css";
+import { GrSearchAdvanced } from "react-icons/gr";
 
 function Explanation() {
   const [titles, setTitles] = useState([
     {
       id: 1,
-      hText: "경제 발전의 상징",
-      sText: "문어는 역시 우주문어가 최고입니다.",
+      hText: "한강의 기적",
+      sText:
+        "6·25전쟁 이후 부터 아시아 금융 위기 시기 까지 반세기에 이르는 급격한 한국 경제 성장의 상징.",
       poster:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnuol0iGuiPIq8PJq2grHX7Fn6nfC3In3JDhseYGhGC3IfVlUODieqWssocDMwMob8NV0&usqp=CAU",
     },
     {
       id: 2,
-      hText: "얼음",
-      sText: "문어는 역시 우주문어가 최고입니다.",
+      hText: "한국의 아름다운 강",
+      sText: "친구, 연인, 가족들의 소풍 성지!",
       poster:
         "https://cdn.crowdpic.net/detail-thumb/thumb_d_D7014A32E2C0FC26CA7DF1F70330BA31.jpg",
     },
