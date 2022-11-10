ecfa55c5f4ee82921d7e1060f80b01dabb8409c6
임수빈 <hange253@gmail.com>
2022-11-10 23:52:52 +0900

    [CREATE] Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Main from "components/Main";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import BookSearch from "./BookSearch";
+import Header from "./common/Header";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <BookSearch />
+    </>
+  );
+}
