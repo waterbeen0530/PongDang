d561cc0ce52f6610a4299f3fb2b250dfa495f979
임수빈 <hange253@gmail.com>
2022-11-11 14:00:46 +0900

    [UPDATE] BookSearch
    add css

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -3,6 +3,7 @@ import search from "../assets/img/Search/search.svg";
 import { ChangeEvent, useState } from "react";
 import { getBookApi } from "util/api/bookApi";
 import { bookType } from "util/interface/book";
+import { ListFormat } from "typescript";
 
 export default function BookSearch() {
   const [title, setTitle] = useState<string>("");
@@ -32,11 +33,26 @@ export default function BookSearch() {
           <img src={search} onClick={getData} />
         </Search>
       </Wrapper>
-      <div>
+      <SearchList>
         {bookData.map((book, i) => (
-          <p key={i}>{book.title}</p>
+          <Book>
+            <img src={book.thumbnail} key={i} />
+            <BookText>
+              <h4>{book.title}</h4>
+              <Info>
+                <p>{book.authors}</p>
+                <p>/</p>
+                <p>{book.publisher}</p>
+              </Info>
+              <Price>
+                <p>{book.price}원</p>
+                <p>{book.sale_price}원</p>
+              </Price>
+              <p>{book.contents}..</p>
+            </BookText>
+          </Book>
         ))}
-      </div>
+      </SearchList>
     </Container>
   );
 }
@@ -62,12 +78,14 @@ const Title = styled.p`
 
 const Search = styled.div`
   position: relative;
+  margin-bottom: 100px;
   input {
     width: 500px;
     height: 46px;
     padding-left: 20px;
     border-radius: 30px;
     border: 1.2px solid #000;
+    outline: none;
     font-size: 16px;
     background-color: #f8f9fa;
   }
@@ -77,5 +95,71 @@ const Search = styled.div`
     right: 20px;
     width: 4.5%;
     cursor: pointer;
+  }
+`;
+
+const SearchList = styled.div`
+  width: 60vw;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Book = styled.div`
+  width: 100%;
+  padding: 40px 0;
+  border-top: 1px solid #000;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  img {
+    width: 130px;
+    height: 180px;
+    margin-right: 20px;
+    border: 0.5px solid #000;
+    filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.25));
+  }
+`;
+
+const BookText = styled.div`
+  h4 {
+    margin: 0 0 16px;
+    font-size: 20px;
+  }
+  p {
+    margin: 0 0 12px;
+  }
+  & > p {
+    width: 70%;
+    font-size: 10px;
+  }
+`;
+
+const Info = styled.div`
+  width: 100%;
+  gap: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 14px;
+`;
+
+const Price = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 14px;
+  p {
+    margin-top: 10px;
+    &:first-child {
+      font-weight: 600;
+    }
+    &:last-child {
+      font-size: 13px;
+      text-decoration: line-through;
+      color: #787e84;
+    }
   }
 `;
